c1764e98bafa087dcbcf38637b4a584cff882256
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-08-08 23:09:08 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyWebsite());
+}
+
+class MyWebsite extends StatelessWidget {
+  const MyWebsite({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Delilah Brao",
+      home: Container(),
+    );
+  }
+}
