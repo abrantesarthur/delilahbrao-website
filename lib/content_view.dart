071d8cb33616c328e09cf43c014cb27c6aa076c4
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-08-08 23:17:43 -0700

    create CustomTab

--- lib/content_view.dart
+++ lib/content_view.dart
@@ -1,8 +1,8 @@
+import 'package:delilahbrao/custom_tab.dart';
 import 'package:flutter/material.dart';
 
 class ContentView {
-  const ContentView({Key? key, required this.tab, required this.content})
-      : super(key: key);
+  const ContentView({Key? key, required this.tab, required this.content});
 
   final CustomTab tab;
   final Widget content;
