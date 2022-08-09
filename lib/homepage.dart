65b68127fd44ef61fd70d8a8d6774d6f420dc194
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-08-08 23:21:36 -0700

    create contentViews list in HomePage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,32 @@
+import 'package:delilahbrao/content_view.dart';
+import 'package:delilahbrao/custom_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  final List<ContentView> contentViews = [
+    ContentView(
+      tab: const CustomTab(title: 'Home'),
+      content: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.blue,
+        ),
+      ),
+    ),
+    ContentView(
+      tab: const CustomTab(title: 'About'),
+      content: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.red,
+        ),
+      ),
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
